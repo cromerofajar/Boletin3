64aae007f0329171f466734fe3e60b10379ed802
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2018-10-23 09:53:48 +0200

    Autoformateo

--- boletin3/ImpDatos.java
+++ boletin3/ImpDatos.java
@@ -1,8 +1,7 @@
-
-
 package boletin3;
 
 public class ImpDatos {
+
     private float km;
     private float litros;
     private float vMed;
@@ -10,46 +9,59 @@ public class ImpDatos {
     private float ceur;
     private float lmed;
 
-    public ImpDatos (){};
-    public ImpDatos (float km, float litros, float vMed, float pGas){
-    this.km=km;
-    this.litros=litros;
-    this.vMed=vMed;
-    this.pGas=pGas;
+    public ImpDatos() {
     }
+
+    ;
+    public ImpDatos(float km, float litros, float vMed, float pGas) {
+        this.km = km;
+        this.litros = litros;
+        this.vMed = vMed;
+        this.pGas = pGas;
+    }
+
     public float getTempo() {
-    float tem= km/vMed;
-    return tem;
+        float tem = km / vMed;
+        return tem;
     }
-    public float consumoMedio (){
-    float cmed =litros/km*100;
-    return cmed;
+
+    public float consumoMedio() {
+        float cmed = litros / km * 100;
+        return cmed;
     }
-    public float consumoEuros (){
-    float cmed =litros/km*100;
-    float eur = pGas*cmed; 
-    return eur;
+
+    public float consumoEuros() {
+        float cmed = litros / km * 100;
+        float eur = pGas * cmed;
+        return eur;
     }
-    public float vMed (){
-    float vmed=vMed;
-    return vmed;}
-    public void setkm(float km){
-    this.km=km;
+
+    public float vMed() {
+        float vmed = vMed;
+        return vmed;
     }
-    public float getKms(){
-    return km;
+
+    public void setkm(float km) {
+        this.km = km;
     }
-    public void setLitros (float lit){
-    this.litros= lit;
+
+    public float getKms() {
+        return km;
     }
-    public float getLitros(){
-    return litros;
+
+    public void setLitros(float lit) {
+        this.litros = lit;
     }
-    public void setpGas (float pGas){
-    this.pGas=pGas;
+
+    public float getLitros() {
+        return litros;
     }
-    public float getpGas (){
-    return pGas;
+
+    public void setpGas(float pGas) {
+        this.pGas = pGas;
     }
-            }  
-    
+
+    public float getpGas() {
+        return pGas;
+    }
+}
